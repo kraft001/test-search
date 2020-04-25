b2ac1a7b09c78e919abd2054367cf62d6f72446f
Konstantin Rafalsky <kraft001@gmail.com>
2020-04-25 12:44:31 +0300

    Configure generators

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,14 @@ module TestSearch
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.test_framework :rspec
+      g.helper_specs false
+      g.controller_specs false
+      g.view_specs false
+      g.routing_specs false
+      g.request_specs false
+    end
   end
 end
