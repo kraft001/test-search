22476eac8c22de5a5c3dd405c007e9b44256be38
Konstantin Rafalsky <kraft001@gmail.com>
2020-04-27 12:25:33 +0300

    Finish the spec for the search service

--- spec/services/search_service_spec.rb
+++ spec/services/search_service_spec.rb
@@ -15,6 +15,8 @@ RSpec.describe SearchService do
     let(:user) { Faker::Lorem.word }
     let(:text) { Faker::Lorem.word }
 
+    let(:channel_name) { "search:#{user}" }
+
     def perform
       described_class.new(user, text).perform
     end
@@ -25,7 +27,7 @@ RSpec.describe SearchService do
       end
 
       it 'does NOT broadcast anything' do
-        expect { perform }.not_to broadcast_to(user)
+        expect { perform }.not_to broadcast_to(channel_name)
       end
 
       it 'creates a search' do
@@ -35,6 +37,41 @@ RSpec.describe SearchService do
       it 'does NOT attach articles to the search' do
         perform
         expect(Search.last.articles.count).to be_zero
+      end
+    end
+
+    context 'when articles were found' do
+      let!(:articles) { create_list :article, 10, title: text }
+
+      it 'responds correctly' do
+        expect(perform).to be_truthy
+      end
+
+      it 'broadcasts one message' do
+        expect { perform }.to broadcast_to(channel_name)
+      end
+
+      it 'creates a search' do
+        expect { perform }.to change(Search, :count).from(0).to(1)
+      end
+
+      it 'attaches all articles to the search' do
+        perform
+
+        expect(Search.last.article_ids).
+          to match_array articles.map(&:id)
+      end
+    end
+
+    context 'when a partial search exists' do
+      let!(:previous_search) do
+        create :search, user: user, text: text[0..-2], partial: false
+      end
+
+      before { perform }
+
+      it 'marks the previous search as partial' do
+        expect(previous_search.reload.partial).to be_truthy
       end
     end
   end
